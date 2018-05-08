f25f27a723168d3fa6b76dd8c013b3dd7a106952
Quant54 <farrahov@gmail.com>
2018-05-08 19:37:04 +0700

    Clear mobile app link[add apple link2]

--- themes/cinderella/header.php
+++ themes/cinderella/header.php
@@ -419,7 +419,7 @@
 	                 $android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
 	                 $palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
 	                 $berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
-	                 $ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+	                 $ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
 	                 $mobile = strpos($_SERVER['HTTP_USER_AGENT'],"Mobile");
 	                 $symb = strpos($_SERVER['HTTP_USER_AGENT'],"Symbian");
 	                 $operam = strpos($_SERVER['HTTP_USER_AGENT'],"Opera M");
@@ -428,7 +428,7 @@
 	                 $winphone = strpos($_SERVER['HTTP_USER_AGENT'],"WindowsPhone");
 	                 $wp7 = strpos($_SERVER['HTTP_USER_AGENT'],"WP7");
 	                 $wp8 = strpos($_SERVER['HTTP_USER_AGENT'],"WP8");
-	                 if ($ipad || $iphone  || $ipod === true) {
+	                 if ($ipad || $iphone ) {
 		                 $applink=$apple;
 	                 }
 	                 if ($android === true) {
